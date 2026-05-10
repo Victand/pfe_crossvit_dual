fafd657d17784193fa59569703ac645a6d98b054
BogaerYouenn <bogaeryouenn@gmail.com>
2026-05-10 16:34:48 +0200

    get_data input incorrect

--- src/pfe_crossvit_dual/training/training_pipeline.py
+++ src/pfe_crossvit_dual/training/training_pipeline.py
@@ -69,7 +69,6 @@ def training_pipeline(config):
     print("Loading data...")
     img_size = CROSSVIT_KWARGS_MAP[config["model"]["crossvit"]]["img_size"]
     train_loader, val_loader, id_to_label = get_data(
-        data_dir=config["dataset"]["data_dir"],
         img_size=img_size,
         batch_size=config["batch_size"],
         **config["dataset"],
